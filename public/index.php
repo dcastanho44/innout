23e1e3640ebe58d2bffb89361117f2b515ca27f2
dcastanho33 <dcastanho33@gmail.com>
2021-08-02 10:24:54 -0300

    gerando comando select

--- public/index.php
+++ public/index.php
@@ -5,7 +5,4 @@ require_once(dirname(__FILE__) . '/../src/config/config.php');
 require_once(dirname(__FILE__) . '/../src/models/User.php');             
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-print_r($user);
-echo '<br><br>';
-$user->email = 'lucas_alterado@gmail.com';
-print_r($user->email);
+echo $user->getSelect();
