6f8cede904853c66959b6543e9932467ea11c157
dcastanho33 <dcastanho33@gmail.com>
2021-08-02 11:36:54 -0300

    tela de autenticação

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 //require_once(dirname(__FILE__) . '/../src/config/database.php');             
 require_once(dirname(__FILE__) . '/../src/config/config.php');             
-require_once(dirname(__FILE__) . '/../src/models/User.php');             
+require_once(dirname(__FILE__) . '/../src/views/login.php');             
 
 foreach(User::get([], 'name') as $user){
     echo $user->name;
