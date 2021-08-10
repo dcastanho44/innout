031d73da0288ea0cbdc8fadc05796a6b56b9df25
dcastanho33 <dcastanho33@gmail.com>
2021-08-10 14:57:09 -0300

    #275

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,10 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt-BR', 'pt-BR.utf-8', 'portuguese');
 
+//Constantes Gerais
+define('DAILY_TIME', 60 * 60 * 8); // 8 horas em segundos
+
+
 // Pastas (constantes para facilitar)
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -1,4 +1,8 @@
 <?php
 session_start();
 requireValidSession();    //só vai continuar caso exista uma sessão válida
-loadTemplateView('day_records');
+
+$date = (new DateTime())->getTimeStamp();
+$today = strftime('%d de %B de %Y', $date);
+
+loadTemplateView('day_records', ['today' => $today]);
