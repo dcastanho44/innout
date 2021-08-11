8111d5dbc9877947f6f1dbf9d4245559419200fb
dcastanho33 <dcastanho33@gmail.com>
2021-08-11 14:29:05 -0300

    #286

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -4,6 +4,10 @@
 loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
-echo '<br>';
 $workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
 print_r($workedIntervalString);
+echo '<br>';
+
+$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchIntervalString);
+echo '<br>';
