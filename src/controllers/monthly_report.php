7ce06bc43f3bbaeb981f104825f1c9a553114bba
dcastanho33 <dcastanho33@gmail.com>
2021-08-13 12:32:02 -0300

    #294

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -3,10 +3,40 @@
 session_start();
 requireValidSession();
 
+$currentDate = new DateTime();
+
 $user = $_SESSION['user'];
 
-$registries = WorkingHours::getMonthlyReport($user->id, new DateTime());
+$registries = WorkingHours::getMonthlyReport($user->id, $currentDate);
+
+$report = [];
+$workday = 0;
+$sumofWorkedTime = 0;
+$lastday = getLastDayOfMonth($currentDate)->format('d');
+
+for ($day = 1; $day <= $lastday; $day++) {
+    $date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+    $registry = $registries[$date];
+
+    if(isPastWorkDay($date)) $workDay++;
+
+    if($registry) {
+        $sumofWorkedTime += $registry->worked_time;
+        array_push($report, $registry);
+    } else {
+        array_push($report, new WorkingHours([
+            'work_date' => $date,
+            'worked-time' => 0
+        ]));
+    }
+}
+
+$expectedTime = $workDay * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumofWorkedTime - $expectedTime));
+$sign = ($sumofWorkedTime >= $expectedTime) ? '+' : '-';
 
 loadTemplateView('monthly_report', [
-    'registries' => $registries
+    'report' => $report,
+    'sumOfWorkedTime' => $sumofWorkedTime,
+    'balance' => "{$sign}{$balance}"
 ]);
